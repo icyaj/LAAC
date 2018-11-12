883ce84fba29e757433c08b2d0da72ebba431e90
Barnaby Collins <barnabyc@sky.com>
2018-11-12 10:40:45 +0000

    Added comments to index and JS code

--- js/navbar-expand.js
+++ js/navbar-expand.js
@@ -1,13 +1,16 @@
 function checkScroll() {
-    if($(window).scrollTop() <= 20 && !expanded) {
-        $(".navbar").css("padding", "1rem 1rem");
-        expanded = true;
+    
+    if($(window).scrollTop() <= 20 && !expanded) {      // if navbar needs to expand
+        $(".navbar").css("padding", "1rem 1rem");       // expand navbar
+        expanded = true;                                // and remember that it's expanded
     }
-    else if($(window).scrollTop() > 30 && expanded) {
-        $(".navbar").css("padding", ".5rem 1rem");
-        expanded = false;
+
+    else if($(window).scrollTop() > 30 && expanded) {   // if navbar needs to contract
+        $(".navbar").css("padding", ".5rem 1rem");      // contract navbar
+        expanded = false;                               // and remember that it's contracted
     }
-    setTimeout(checkScroll, 10);
+
+    setTimeout(checkScroll, 10);                        // check again in 10ms
 }
-var expanded = true;
-checkScroll();
+var expanded = true;    // initialise expanded
+checkScroll();          // check for the first time
